5e2acb9fbdc0a74c4a39d5a062a4895c788c34af
Davis Too <daviskipkiruitoo@gmail.com>
2023-11-19 10:19:53 +0300

    added tv api and job

--- app/Jobs/ParseStreamUrls.php
+++ app/Jobs/ParseStreamUrls.php
@@ -8,6 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Http;
 
 class ParseStreamUrls implements ShouldQueue
 {
@@ -27,5 +28,23 @@ class ParseStreamUrls implements ShouldQueue
     public function handle(): void
     {
         //
+        $response = Http::get('https://sportsonline.gl/prog.txt');
+        // echo($response->body());
+
+        $programms = $response->body();
+
+        $linearray = preg_split('/\r\n|\r|\n/',$programms);
+
+        $urlarrays = preg_grep("/^[0-9]*/", $linearray);
+
+        $urls = array();
+
+        foreach ($urlarrays as $url){
+            $a = explode("|", $url);
+
+            array_push($urls, $a);
+        }
+
+        echo json_encode($urls);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,33 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/tv', function (Request $request){
+    $response = Http::get('https://sportsonline.gl/prog.txt');
+    // echo($response->body());
+
+    $programms = $response->body();
+
+    $linearray = preg_split('/\r\n|\r|\n/',$programms);
+
+    $urlarrays = preg_grep("/^[0-9].*/", $linearray);
+
+    $urls = array();
+
+    foreach ($urlarrays as $url){
+        $a = explode("| ", $url);
+
+        $sm_array = explode("   ",$a[0]);
+
+        $fa = [
+            "time" => $sm_array[0],
+            "title" => $sm_array[1],
+            "url" => end($a)
+        ];
+
+        array_push($urls, $fa);
+    }
+
+    return response()->json($urls);
 });
